92781a2aaf7172526d981746a2d3a583d07ac29b
bharti <techjnr@inciterz.com>
2025-09-11 23:22:30 +0530

    change app,js but workfine

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+
+// // project-import
+// import renderRoutes, { routes } from './routes';
+
+// // ==============================|| APP ||============================== //
+
+// const App = () => {
+//   return <BrowserRouter basename={process.env.REACT_APP_BASE_NAME || '/'}>{renderRoutes(routes)}</BrowserRouter>;
+// };
+
+// export default App;
+
+//bharti
+// App.js
 
 // project-import
 import renderRoutes, { routes } from './routes';
@@ -7,7 +21,7 @@ import renderRoutes, { routes } from './routes';
 // ==============================|| APP ||============================== //
 
 const App = () => {
-  return <BrowserRouter basename={process.env.REACT_APP_BASE_NAME || '/'}>{renderRoutes(routes)}</BrowserRouter>;
+  return renderRoutes(routes); // no BrowserRouter here
 };
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,45 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+
+// // styles
+// import './index.scss';
+
+// // projct import
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { ConfigProvider } from './contexts/ConfigContext';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(
+//   <ConfigProvider>
+//     <App />
+//   </ConfigProvider>
+// );
+
+// reportWebVitals();
+
+//bharti
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 // styles
 import './index.scss';
 
-// projct import
+// project imports
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ConfigProvider } from './contexts/ConfigContext';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
+
 root.render(
   <ConfigProvider>
-    <App />
+    <BrowserRouter basename="">
+      <App />
+    </BrowserRouter>
   </ConfigProvider>
 );
 
